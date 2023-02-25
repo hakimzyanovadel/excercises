132400aa87e736a5c8fa4fb0c36a864db72cbae9
Adel <Adel@CHEZAMONSTER>
2023-02-25 13:53:30 +0300

    Добавьте файлы проекта.

---
+++ ex2/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace ex2
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int n1, n2, n3, n4, n5, n6, n7, result;
+            Console.WriteLine("Введите число n1");
+            n1 = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("Введите число n2");
+            n2 = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("Введите число n3");
+            n3 = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("Введите число n4");
+            n4 = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("Введите число n5");
+            n5 = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("Введите число n6");
+            n6 = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("Введите число n7");
+            n7 = Convert.ToInt32(Console.ReadLine());
+            int[] mas = new int[7] { n1, n2, n3, n4, n5, n6, n7 };
+            Console.WriteLine("Максимальное число =" + mas.Max());
+        }
+    }
+}
---
+++ excercises/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace excercises
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int a;
+            double firstNumber, secondNumber, thirdNumber, fourthNumber;
+            Console.WriteLine("Введите число");
+            a = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("Исходное число - " + a);
+            if (a.ToString().Length == 4)
+            {
+                firstNumber = a % 10;
+                Console.WriteLine(firstNumber);
+                secondNumber = a / 10 % 10;
+                Console.WriteLine(secondNumber);
+                thirdNumber = a / 100 % 10;
+                Console.WriteLine(thirdNumber);
+                fourthNumber = a / 1000;
+                Console.WriteLine(fourthNumber);
+                Console.WriteLine("Перевернутое значение - " + firstNumber.ToString() + secondNumber.ToString() + thirdNumber.ToString() + fourthNumber.ToString());
+            }
+            else
+            {
+                Console.WriteLine("Вы ввели не четырехзначное число");
+            }
+        }
+    }
+}
